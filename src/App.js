26b7e9cba3b583289078e1de0ae74ff8d35e96e3
Julian Krispel <julian@reactrocket.com>
2018-06-19 17:11:39 +0100

    remove junk

--- src/App.js
+++ src/App.js
@@ -46,10 +46,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
         <RenderObservable
           observable={this._obs$}
         >
@@ -65,9 +61,6 @@ class App extends Component {
             </RenderObservable>
           </div>}
         </RenderObservable>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
